680e4b2586f2e3568654533f483891e281b291d1
mdrakib155 <79098633+mdrakib155@users.noreply.github.com>
2021-03-14 21:14:42 +0600

    Background image and logo added

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((360,640))
 
+#Background image
+
+background = pygame.image.load('background.png')
+
+#title and logo
+
+pygame.display.set_caption("C.Vacc")
+icon = pygame.image.load('logo.png')
+pygame.display.set_icon(icon)
+
+
+#Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #screen.fill((255, 0, 0))
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+
